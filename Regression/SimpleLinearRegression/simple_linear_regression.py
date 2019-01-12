e990b597faca90e16b461b161020534589fa2905
Muhammed Salman Shamsi <muhammed-salman@users.noreply.github.com>
2019-01-12 09:44:36 +0530

    Test Data Plot Modified

--- Regression/SimpleLinearRegression/simple_linear_regression.py
+++ Regression/SimpleLinearRegression/simple_linear_regression.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 #importing dataset
 dataset=pd.read_csv('Salary_Data.csv')
 X=dataset.iloc[:,:-1].values
-y=dataset.iloc[:,1].values
+y=dataset.iloc[:,-1:].values
 
 #splitting the dataset into the training and test sets
 from sklearn.model_selection import train_test_split #used model_selection in place of cross_validation since the latter is deprecated
@@ -33,8 +33,8 @@ plt.show()
 
 #visualising the test set results
 plt.scatter(X_test, y_test, color='red')
-plt.plot(X_train, regressor.predict(X_train), color='blue')
-plt.title('Salary Vs Experience (Training set)')
+plt.plot(X_test, regressor.predict(X_test), color='blue')
+plt.title('Salary Vs Experience (Test set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
